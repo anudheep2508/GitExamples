8734063d36738f7091d2cf2dcd8a389338b9b0ee
anudheep2508 <anudheepravichandran@gmail.com>
2022-12-01 09:52:18 +0530

    file deleted

--- testbatch.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\sakha\.eclipse\Salenium
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*;
-java org.testng.TestNG NewTestHero1.xml
-pause
